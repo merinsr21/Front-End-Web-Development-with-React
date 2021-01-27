137e690cd83bc98a5ce25668ef93b724928237f1
merinsr21 <merinsr21@gmail.com>
2021-01-27 00:18:40 -0500

    sigle page applications

--- confusion/src/components/MainComponent.js
+++ confusion/src/components/MainComponent.js
@@ -2,7 +2,11 @@ import React, { Component } from 'react';
 import Home from './HomeComponent';
 import Menu from './MenuComponent';
 import { DISHES } from '../shared/dishes';
+import { COMMENTS } from '../shared/comments';
+import { LEADERS } from '../shared/leader';
+import { PROMOTIONS } from '../shared/promotions';
 import DishDetail from './DishdetailComponent';
+import Contact from './ContactComponent';
 import Header from './HeaderComponent';
 import Footer from './FooterCompoent';
 import { Switch, Route, Redirect} from 'react-router-dom';
@@ -14,14 +18,19 @@ class Main extends Component {
 
         this.state = {
             dishes: DISHES,
-            selectedDish: null
+            comments: COMMENTS,
+            promotions: PROMOTIONS,
+            leaders: LEADERS
         };
     }   
 
     render() {
         const HomePage = () => {
             return(
-                <Home />
+                <Home dish={this.state.dishes.filter((dish) => dish.featured)[0]}
+                promotion={this.state.promotions.filter((promo) => promo.featured)[0]}
+                leader={this.state.leaders.filter((leader) => leader.featured)[0]}
+                />
             );
         }
         
@@ -31,6 +40,7 @@ class Main extends Component {
                 <Switch>
                     <Route path="/home" component={HomePage} />
                     <Route exact path="/menu" component={() => <Menu dishes={this.state.dishes}/> } />
+                    <Route exact path="/contactus" component={Contact} />
                     <Redirect to="/home" />
                 </Switch>
                 <Footer />
